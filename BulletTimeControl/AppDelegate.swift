5f8e40a041809b18c47fcce5a88324c151fd31a3
Ben Rigas <ben@rigas.net>
2017-07-03 07:39:58 -0400

    Initial Commit

---
+++ BulletTimeControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BulletTimeControl
+//
+//  Created by Ben Rigas on 7/3/17.
+//  Copyright © 2017 Rigas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
