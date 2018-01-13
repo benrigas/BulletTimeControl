5a6cd44f345d7e0da79c09897e4d4e7f588745fa
Ben Rigas <benrigas@Bens-MacBook-Pro.local>
2018-01-13 17:40:23 -0500

    changes

--- gif2movie.sh
+++ gif2movie.sh
@@ -2,4 +2,4 @@
 
 cd $1
 
-ffmpeg -i capture.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" capture.mp4
+/usr/local/bin/ffmpeg -i capture.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" capture.mp4
