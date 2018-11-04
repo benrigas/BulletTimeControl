b99db9064ba3cf2aa164eb70c599a5e15e5b8dec
Ben Rigas <benrigas@Bens-MBP.localdomain>
2018-11-04 15:17:40 -0500

    Adding cal file start

---
+++ BulletTimeControl/CameraCalibrator.swift
@@ -0,0 +1,13 @@
+//
+//  CameraCalibrator.swift
+//  BulletTimeControl
+//
+//  Created by Ben Rigas on 11/4/18.
+//  Copyright © 2018 Rigas. All rights reserved.
+//
+
+import Cocoa
+
+class CameraCalibrator: NSObject {
+
+}
